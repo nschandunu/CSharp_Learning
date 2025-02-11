8e87569e78926c81c4784c2677880fd0b069477b
Senuka Chandunu <senukaacc@gmail.com>
2025-02-11 21:04:59 +0530

    Simple Hello world program C#

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        PrintMessage("Hello, World!", 5);
+    }
+
+    static void PrintMessage(string message, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+    
+    
+    
