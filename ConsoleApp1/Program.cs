a674898c04a08d4a5ad391c9b947bbfa814a194b
Senuka Chandunu <senukaacc@gmail.com>
2025-02-12 13:30:31 +0530

    HW with read line

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class program
+    class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 100; i++)
-            {
-                Console.WriteLine("Hello World!");
-            }
+            Console.WriteLine ("Hello World!");
+            Console.ReadLine();
         }
     }
 }
