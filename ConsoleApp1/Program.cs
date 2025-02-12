c97a92d07d18bdea30a62603ace95ff74422f729
Senuka Chandunu <senukaacc@gmail.com>
2025-02-12 19:45:54 +0530

    Coding C# using terminal VIM & Mono compiler

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Namespace is a way to organize the code. It is a collection of classes, 
 namespace ConsoleApp1
 {
     class Program
@@ -11,9 +12,14 @@ namespace ConsoleApp1
         static void Main(string[] args)
 
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 1; i < 21; i++)
             {
                 Console.WriteLine("This is " + i + " line");
+            }
+
+            {
+                Console.WriteLine(2+2);
+                Console.WriteLine("2"+"3");
             }
         }
     } 
