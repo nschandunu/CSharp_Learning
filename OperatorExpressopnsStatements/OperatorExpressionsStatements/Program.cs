c6b6eb7ebec25a36294f9f5515c3b2d12cde4023
Senuka Chandunu <senukaacc@gmail.com>
2025-02-15 14:09:14 +0530

    Operator Expressions

---
+++ OperatorExpressopnsStatements/OperatorExpressionsStatements/Program.cs
@@ -0,0 +1,41 @@
+﻿// Variable Declaration
+            int x, y, a, b;
+            
+            // Assignment Operator
+            x = 3;
+            y = 2;
+            a = 1;
+            b = 0;
+            
+            // Addition //sub //multiplication
+            x = 3 + 4;
+            x = 4 - 3;
+            x = 10 * 5;
+            x = 10 / 5;
+            
+            // order of operations using parenthesis
+            x = (x + y) * (a - b);
+            
+            // Equality operator
+            if (x == y)
+            {
+                
+            }
+            
+            // There are two "conditional" operators as well that can
+            // be used to expand / enhance an evaluation ...
+            // ... and they can be combined together multiple times.
+            // Conditional AND operator ...
+            if ((x › y) && (a › b))
+            {
+            }
+            // Conditional OR operator ...
+            if ((x > y) || (a › b))
+            {
+            }
+            
+            // Also, here's the in-line conditional operator we
+            // learned about in the previous lesson ...
+            string message = (x == 1) ? "Car" : "Boat";
+            // Member access and Method invocation
+                        Console.WriteLine ("Hi");
