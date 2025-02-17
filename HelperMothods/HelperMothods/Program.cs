f0025787867fffadc1c2fc4640226c5fe40ad89b
Senuka Chandunu <senukaacc@gmail.com>
2025-02-17 19:22:02 +0530

    Helper Methods

---
+++ HelperMothods/HelperMothods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("The Name Game");
+                
+                Console.WriteLine("What's your first name? ");
+                string firstName = Console.ReadLine();
+                
+                Console.Write("What's your last name? ");
+                string lastName = Console.ReadLine();
+                
+                Console.Write("In what city were you born?");
+                string city = Console. ReadLine();
+                
+                
+                
+            }
+        }
+}
---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                HelloWorld();
+            }
+
+            private static void HelloWorld()
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+}
