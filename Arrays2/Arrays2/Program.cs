9f7f8eab2dc7e0eeee1eb22eb7eaebc84d5319f9
Senuka Chandunu <senukaacc@gmail.com>
2025-02-18 22:54:32 +0530

    Helper Methods  & Arrays ( 2 )

---
+++ Arrays2/Arrays2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // method 1
+                string[] car = {"BMW", "Benz", "Audi", "Porsche"};
+                int[] ages = {20, 30, 40, 50};
+                string[] names = new String[3]{"John", "Doe", "Smith"};  // specially mention element count
+                int[] age = new int[2] { 21, 22 };
+
+                // change array value
+                car[0] = "Toyota";
+                
+                Console.WriteLine(car[0]);
+                Console.WriteLine(car[2]);
+                Console.WriteLine(car.Length);
+                Console.WriteLine(age[1]);
+                
+                Array.Reverse(car);
+                Console.WriteLine(car[0]);
+
+            }
+        }
+}
--- HelperMothods/HelperMothods/Program.cs
+++ HelperMothods/HelperMothods/Program.cs
@@ -12,16 +12,42 @@ namespace HelperMethods
             {
                 Console.WriteLine("The Name Game");
                 
-                Console.WriteLine("What's your first name? ");
+                Console.Write("What's your first name? ");
                 string firstName = Console.ReadLine();
                 
                 Console.Write("What's your last name? ");
                 string lastName = Console.ReadLine();
                 
-                Console.Write("In what city were you born?");
+                Console.Write("In what city were you born? ");
                 string city = Console. ReadLine();
                 
+                char[] firstNameArray = firstName.ToCharArray();
+                Array.Reverse(firstNameArray);
                 
+                char[] lastNameArray = lastName.ToCharArray();
+                Array.Reverse(lastNameArray);
+                
+                char[] cityArray = city.ToCharArray();
+                Array.Reverse(cityArray);
+                
+                string result = "";
+                
+                foreach (char item in firstNameArray)
+                {
+                    result += item;
+                }
+                
+                foreach (char item in lastNameArray)
+                {
+                    result += item;
+                }
+                result += " ";
+                foreach (char item in cityArray)
+                {
+                    result += item;
+                }
+                
+                Console.WriteLine("Results: " + result);
                 
             }
         }
